dadcbbb7e9d1497619cea8334f281b4f2fdda03e
ohhaus <ohhaus84@gmail.com>
2025-07-16 00:56:10 +0300

    Remove unused API files and update settings, URLs, and requirements for improved project structure

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
+    # Admin
     path('admin/', admin.site.urls),
+
+    # API
+    path('api/', include('recipes.urls')),
+    path('api/', include('users.urls')),
 ]
