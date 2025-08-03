7607b178272a0be97a7fe479e15e0aa4dd384253
ohhaus <ohhaus84@gmail.com>
2025-08-03 04:50:42 +0300

    hmmm..flake8

--- backend/tests/test_recipes.py
+++ backend/tests/test_recipes.py
@@ -11,7 +11,7 @@ class TestRecipeAPI:
         """Тест получения списка рецептов без авторизации."""
         url = reverse('api:recipe-list')
         response = client.get(url)
-        
+
         assert response.status_code == status.HTTP_200_OK
         assert isinstance(response.data.get('results'), list)
 
@@ -27,9 +27,9 @@ class TestRecipeAPI:
                 {'id': 1, 'amount': 100}
             ]
         }
-        
+
         response = auth_client.post(url, payload, format='json')
-        
+
         assert response.status_code == status.HTTP_201_CREATED
         assert Recipe.objects.filter(name='Тестовый рецепт').exists()
 
@@ -37,7 +37,7 @@ class TestRecipeAPI:
         """Тест получения детальной информации о рецепте."""
         url = reverse('api:recipe-detail', kwargs={'id': recipe.id})
         response = client.get(url)
-        
+
         assert response.status_code == status.HTTP_200_OK
         assert response.data['name'] == recipe.name
         assert response.data['cooking_time'] == recipe.cooking_time
@@ -54,9 +54,9 @@ class TestRecipeAPI:
                 {'id': 1, 'amount': 100}
             ]
         }
-        
+
         response = auth_client.patch(url, payload, format='json')
-        
+
         assert response.status_code == status.HTTP_200_OK
         recipe.refresh_from_db()
         assert recipe.name == 'Обновленный рецепт'
--- backend/tests/test_tags.py
+++ backend/tests/test_tags.py
@@ -9,7 +9,7 @@ class TestTagAPI:
         """Тест получения списка тегов."""
         url = reverse('api:tag-list')
         response = client.get(url)
-        
+
         assert response.status_code == status.HTTP_200_OK
         assert isinstance(response.data, list)
 
@@ -17,7 +17,7 @@ class TestTagAPI:
         """Тест получения информации о конкретном теге."""
         url = reverse('api:tag-detail', kwargs={'id': tag.id})
         response = client.get(url)
-        
+
         assert response.status_code == status.HTTP_200_OK
         assert response.data['name'] == tag.name
         assert response.data['slug'] == tag.slug
